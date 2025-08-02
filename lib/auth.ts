fd8544818bb0bbe27d0d524929d698107a170e30
sasch040 <ftown4446@gmail.com>
2025-08-02 22:23:35 +0200

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,26 +1,3 @@
-// lib/types.ts
-
-export interface User {
-  id: number;
-  username: string;
-  email: string;
-}
-
-export interface AuthResponse {
-  jwt: string;
-  user: User;
-}
-
-export interface ModuleProgress {
-  id: number;
-  module: number;
-  videoWatched: boolean;
-  quizCompleted: boolean;
-  completed: boolean;
-}
-
-// lib/auth.ts
-
 const TOKEN_KEY = 'token';
 
 export function getToken(): string | null {
